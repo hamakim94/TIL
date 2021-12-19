983300f95c32da2b19c862a07e82280f73ced9b0
hama <mingyun0324@gmail.com>
2021-12-20 00:02:07 +0900

    21/12/19 Algorithm

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,17 +1,18 @@
+import sys
+sys.stdin = open('C:/Users/mingy/OneDrive/바탕 화면/Algorithm/input.txt', 'rt')
 
+n = int(sys.stdin.readline())
 
-a = set(range(1, 101))
-gen_num = set()
-# 필요한 기능, 자릿수 더해서 삭제..
+cnt = 0
+for i in range(1,n+1):
+    k = str(i)
+    if len(k) <= 2:
+        cnt += 1
+    else:
+        t = set()
+        for j in range(len(k)-1):
+            t.add( int(k[j]) - int(k[j+1]))
+        if len(t) == 1:
+            cnt += 1
 
-for i in range(1,101):
-    t = 0
-    for j in str(i):
-        i += int(j)
-    gen_num.add(i)
-    
-ans = sorted(a - gen_num)
-for i in ans:
-    print(i)
-
-        
+print(cnt)
