22698b9e631ff6a14be09a86eaee9fb88196fb3b
Hama <mingyun0324@gmail.com>
2021-12-22 16:33:35 +0900

    21/12/22 Algorithm

--- algorithm_test.py
+++ algorithm_test.py
@@ -2,30 +2,13 @@ import sys
 sys.stdin = open('C:/Users/User/Desktop/Algorithm/input.txt', 'rt')
 
 k = int(input())
-res = 0
 for _ in range(k):
-    n = input()
-    stack = []
-    ans = []
-    for i in n:
-        if not stack:
-            stack.append(i)
-            ans.append(i)
-        if i == stack[0]:
-            continue
-        else:
-            stack.pop()
-            stack.append(i)
-            ans.append(i)
-
-    if len(ans) == len(set(ans)):
-        res += 1
-print(res)
-# 내 첫 생각
-# 1. 중복제거(여기서부터 어려워), 리스트에 넣기
-# 2. set 하기
-# 3. 개수 같으면 += 1
-
-
-
-
+    h, w, n = map(int, input().split())
+    room_num = n // h
+    height = n % h
+    
+    if n%h == 0:
+        print(h, "{0:02d}".format(room_num), sep = '')
+    else:
+        print(height, "{0:02d}".format(room_num + 1), sep = '')
+    
