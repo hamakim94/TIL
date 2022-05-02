dd8f8ae3114d49c92941fc7ea59e3ada6d35b5fb
hamakim94 <mingyun0324@gmail.com>
2022-05-02 22:47:15 +0900

    22/05/02 vue&aps

---
+++ 07_vue/prac/HW/2.js
@@ -0,0 +1,39 @@
+let name = "Kim";
+let birthday = "2000.01.01";
+let job = "Student";
+
+let info = `내 이름은 ${name}이고 ${birthday}에 태어 났습니다. 제 직업은 ${job}입니다`
+console.log(info)
+
+let Movie = {
+    title : "기생충",
+    director : "봉준호",
+    genre : "드라마",
+    runningTime : 132
+}
+// key값이랑 변수명이랑 이름 같아야지 됨, 다르더니 안 되더라..
+let {title,director,genre,runningTime} = Movie;
+console.log(title);
+console.log(director);
+console.log(genre);
+console.log(runningTime);
+
+// 반복문을 통해 영화 배열 출력?
+let Movies = ["기생충", "이터널스", "킹스맨", "스파이더맨", "해리포터"];
+
+Movies.forEach(movie => console.log(movie));
+
+// 인자로 숫자 1개 받아, 세제곱을 반환 => 화살표
+function triple(a) {
+    return a**3;
+}
+
+let ans = a => a**3;
+console.log(ans(3))
+
+// Arrays Helper Methods중 map을 메서드를 사용하여 
+// 임의의 정수를 가진 배열의 요소를 2배씩 하여 새로운 배열 생성
+
+let tempArr = [1,4,3,2,6,4];
+let timesTwo = tempArr.map(x => x*2);
+console.log(timesTwo)
