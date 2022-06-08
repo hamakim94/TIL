45bdfbdd7aaf50581d9a622ccfec6bad4dd26fb1
hamakim94 <mingyun0324@gmail.com>
2022-06-08 09:53:28 +0900

    22/06/08 study aps

---
+++ 02_APS/HW/backjoon/BOJ_220608/BOJ_25083_새싹.java
@@ -0,0 +1,13 @@
+package BOJ_220608;
+
+public class BOJ_25083_새싹 {
+	public static void main(String[] args) {
+		String now = "         ,r'\"7\n" + 
+				"r`-_   ,'  ,/\n" + 
+				" \\. \". L_r'\n" + 
+				"   `~\\/\n" + 
+				"      |\n" + 
+				"      |";
+		System.out.print(now);
+	}
+}
