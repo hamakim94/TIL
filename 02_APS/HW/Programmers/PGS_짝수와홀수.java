8a8c7b6532713da3e6677ee0a948ca09901b66a5
김민균 <mingyun0324@gmail.com>
2022-09-08 20:59:20 +0900

    22/09/08 study aps

---
+++ 02_APS/HW/Programmers/PGS_짝수와홀수.java
@@ -0,0 +1,15 @@
+
+public class PGS_Â¦¼ö¿ÍÈ¦¼ö {
+	class Solution {
+	    public String solution(int num) {
+	        String answer = "";
+	        if (num % 2 == 0){
+	            answer = "Even";
+	        } else{
+	            answer = "Odd";
+	        }
+	        
+	        return answer;
+	    }
+	}
+}
