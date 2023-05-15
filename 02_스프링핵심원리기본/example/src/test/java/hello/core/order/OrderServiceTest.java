a7b123539e1e4b89b4366956b3be6a83e0c6aa46
김민균 <mingyun0324@gmail.com>
2023-05-15 17:23:51 +0900

    스프링핵심원리_기본편_섹션7_의존관계자동주입_조회 빈이 2개 이상 - 문제

--- 02_스프링핵심원리기본/example/src/test/java/hello/core/order/OrderServiceTest.java
+++ 02_스프링핵심원리기본/example/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,11 +1,9 @@
 package hello.core.order;
 
 import hello.core.AppConfig_before;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
