44e62c3d193a230a9f5d620c25b21e218ea20bc8
김민균 <mingyun0324@gmail.com>
2023-05-15 17:36:24 +0900

    스프링핵심원리_기본편_섹션7_의존관계자동주입_@Autowired 필드명, @Qualifier, @Primary(여러 빈 선택될 떄 어떻게 해결)

--- 02_스프링핵심원리기본/example/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02_스프링핵심원리기본/example/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,13 +4,14 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements  OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy; // 이렇게 하면 추상화된 인터페이스만 의존하도록 설계했음!
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,  DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
