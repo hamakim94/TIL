b6521955c9669c2b91bd964489c700a359b34ddd
김민균 <mingyun0324@gmail.com>
2023-05-15 17:18:42 +0900

    스프링핵심원리_기본편_섹션7_의존관계자동주입_롬복과 최신 트랜드

--- 02_스프링핵심원리기본/example/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 02_스프링핵심원리기본/example/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,18 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
+@RequiredArgsConstructor  // final이 붙은 거에 생성자를 자동으로 만들어줌..
 public class OrderServiceImpl implements  OrderService{
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // 이렇게 하면 추상화된 인터페이스만 의존하도록 설계했음!
 
-    private  final MemberRepository memberRepository;
-    private  final DiscountPolicy discountPolicy; // 이렇게 하면 추상화된 인터페이스만 의존하도록 설계했음!
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
