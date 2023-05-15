c15f587e743661c0bbc5e8e62659f644f92d72a6
김민균 <mingyun0324@gmail.com>
2023-05-15 10:34:17 +0900

    스프링핵심원리_기본편_섹션4_다양한설정혁식지원_자바코드_XML

---
+++ 02_스프링핵심원리기본/example/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        // classpath, 즉 resource 아래에서 읽는다
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
