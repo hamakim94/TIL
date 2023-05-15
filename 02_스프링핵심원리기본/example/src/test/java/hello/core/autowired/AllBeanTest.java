db5102e93b9d3823a421563eb14780ec98a08448
김민균 <mingyun0324@gmail.com>
2023-05-16 08:05:54 +0900

    스프링핵심원리_기본편_섹션7_의존관계자동주입_ 조회한 빈이 모두 필요할 떄, List, Map

---
+++ 02_스프링핵심원리기본/example/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,52 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "uesrA", Grade.VIP);
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    //AutoAppConfig에서 자동으로 Component scan해서 등록된 bean들을, 해당 ac에서 자동으로 등록해줌
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
