f62fe0eea334ce484487b93f01727380b1d63d43
김민균 <mingyun0324@gmail.com>
2023-05-17 10:05:43 +0900

    스프링핵심원리_기본편_섹션9_빈 스코프_스코프와 Provider

--- 02_스프링핵심원리기본/example/src/main/java/hello/core/web/LogDemoController.java
+++ 02_스프링핵심원리기본/example/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,13 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
+
+        Thread.sleep(1000); // 원래는 요청이 엄청 섞이니까, 스레드를 통해 오래 걸리는 서비스를 가정, 새로고침 연속 4번
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
