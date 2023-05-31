013bb86fe89cb7ed893eedad7918838fa4c1c03f
김민균 <mingyun0324@gmail.com>
2023-05-31 17:22:10 +0900

    230531_ JPA활용1_섹션2_도메인분석설계

--- 04_실전! 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ 04_실전! 스프링 부트와 JPA 활용 1/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,12 +8,8 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-
-		System.out.println("data = " + data);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
 }
+;
