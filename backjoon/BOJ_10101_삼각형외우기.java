7b6e1339954a837384ba779a844d73cc7e36b5d9
김민균 <mingyun0324@gmail.com>
2023-08-20 16:56:33 +0900

    study aps

---
+++ backjoon/BOJ_10101_삼각형외우기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10101_삼각형외우기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a + b + c == 180) {
+            if (a == 60 && b == 60 && c == 60) {
+                System.out.println("Equilateral");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Isosceles");
+            } else{
+                System.out.println("Scalene");
+            }
+
+        } else{
+            System.out.println("Error");
+        }
+
+    }
+}
