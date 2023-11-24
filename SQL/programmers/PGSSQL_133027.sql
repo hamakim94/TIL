11dbda38fd191372521cd085bf88f5fa4dd06836
김민균 <mingyun0324@gmail.com>
2023-11-24 09:45:18 +0900

    study sql - join, union

---
+++ SQL/programmers/PGSSQL_133027.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT  FLAVOR
+FROM 
+    (SELECT *
+    FROM FIRST_HALF 
+    UNION 
+    SELECT *
+    FROM JULY ) as S
+GROUP BY S.FLAVOR
+ORDER BY SUM(S.TOTAL_ORDER) desc
+limit 3;
