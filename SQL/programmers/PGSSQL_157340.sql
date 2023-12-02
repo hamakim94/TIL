cfe1c7dc090bd6dff8474826448759dffdcfd35f
김민균 <mingyun0324@gmail.com>
2023-12-02 19:06:55 +0900

    study sql ~ between, group by

---
+++ SQL/programmers/PGSSQL_157340.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT CAR_ID, 
+    MAX(
+        CASE
+            WHEN '2022-10-16' BETWEEN start_date  AND end_date
+            THEN '대여중'
+        ELSE '대여 가능'
+    END) AS AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
+
+
