2d6faa758ce4f44ef64312d6b9d81ab971f41584
김민균 <mingyun0324@gmail.com>
2023-12-04 10:52:56 +0900

    study sql ~ datediff

---
+++ SQL/programmers/PGSSQL_151138.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT HISTORY_ID, CAR_ID, date_format(START_DATE,'%Y-%m-%d'),  date_format(END_DATE, '%Y-%m-%d'),
+CASE
+    WHEN datediff(END_DATE, START_DATE) >= 29 THEN '장기 대여'
+    ELSE '단기 대여'
+END AS RENT_TYPE
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE MONTH(START_DATE) = 9
+ORDER BY HISTORY_ID DESC;
